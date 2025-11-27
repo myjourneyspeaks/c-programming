ecf674b2ad4dd81fce2115b031b7692f729956d6
myjourneyspeaks <lindelwekhumalo@outlook.com>
2025-11-27 19:52:37 +0200

    condtion statements

---
+++ projects/project seven/build.sh
@@ -0,0 +1,1 @@
+gcc -o nested-if main.c
---
+++ projects/project seven/main.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+
+  float price = 10.00;
+  bool isStudent = true; // 10% discount
+  bool isSenior = true;  // 20% discount
+
+  if (isStudent) {
+    if (isSenior) {
+      printf("You get a student discount of 10%%\n");
+      printf("You get a senior discount of 20%%\n");
+      price *= 0.7;
+    } else {
+      printf("You get a student discount of 10%%\n");
+      price *= 0.9;
+    }
+  } else {
+    if (isSenior) {
+      printf("You get a senior discount of 20%%\n");
+      price *= 0.8;
+    }
+  }
+
+  printf("The price of a ticket is: $%.2f\n", price);
+
+  return 0;
+}
