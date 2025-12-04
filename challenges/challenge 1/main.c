7e7ee05560cb261213897a93896bbd5e9919289d
myjourneyspeaks <lindelwekhumalo@outlook.com>
2025-12-04 23:36:00 +0200

    challenge 1 done

--- challenges/challenge 1/main.c
+++ challenges/challenge 1/main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  // challenge one: given a = 5, x = 1, and y = 5, create a program that outputs
-  // the result of the formula f = (a - b)(x - y).
 
+  // Declaration
   int a = 5;
   int x = 1;
   int y = 5;
