1dd39e42b861d5f05e4c61af5ae520b5f04615a0
myjourneyspeaks <lindelwekhumalo@outlook.com>
2025-12-13 22:17:00 +0200

    fixed

---
+++ learning_c/enum_2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+typedef enum { SUCCESS, FAILURE, PENDING } Status;
+
+// prototype
+void connectStatus(Status status);
+
+int main() {
+
+  Status status = PENDING;
+
+  connectStatus(status);
+
+  return 0;
+}
+
+void connectStatus(Status status) {
+  switch (status) {
+  case SUCCESS:
+    printf("Connection was Successful\n");
+    break;
+  case FAILURE:
+    printf("Could not connect\n");
+    break;
+  case PENDING:
+    printf("Connecting...\n");
+    break;
+  }
+}
