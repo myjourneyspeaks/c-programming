55b693c59e1204ebf720ebe71b84e50eb3664654
myjourneyspeaks <lindelwekhumalo@outlook.com>
2026-01-25 02:12:56 +0200

    reright

---
+++ learning_c/variables and datatypes/1. integer/main.c
@@ -0,0 +1,25 @@
+/*
+ * Program: interger
+ * Purpose: Learning interger dataype
+ * Author: Vusumuzi Lindelwe
+ * Date: 2026-01-25
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+  // Intergers are whole numbers that represents positive and negative numbers
+  // such as -3, -4, -2, 2, 9, 10. Intergers hold a maximum of 4 bytes of
+  // information
+
+  int age = 23;
+  int year = 2026;
+  int quantity = 35;
+
+  printf("You are %d years old.\n", age);
+  printf("This year is %d.\n", year);
+  printf("Your quantity %d.\n", quantity);
+
+  return 0;
+}
