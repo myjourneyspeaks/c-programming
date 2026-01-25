e1f860c3c7571b8991980cef66d51105aea92d7f
myjourneyspeaks <lindelwekhumalo@outlook.com>
2026-01-25 16:13:43 +0200

    arithmetic Operation

--- learning_c/arithmetic operators/main.c
+++ learning_c/arithmetic operators/main.c
@@ -9,21 +9,31 @@
 int main(void) {
 
   // arithmetic operators = + - * / % ++ --
-  int x = 20;
-  int y = 15;
-  int z = 0;
+  // int x = 20;
+  // int y = 15;
+  // int z = 0;
   // float y = 15;
   // float z = 0;
 
-  z = x + y;
-  z = x - y;
-  z = x * y;
-  z = x / y;
-  z = x % y;
-  x++;
-  y--;
+  int x = 10;
+  int y = 2;
+  int z = 0;
 
-  printf("%d\n", z);
+  // z = x + y;
+  // z = x - y;
+  // z = x * y;
+  // z = x / y;
+  // z = x % y;
+  // x++;
+  // y--;
+
+  // === augmented assignment operators ===
+  x += 2;
+  x -= 3;
+  x *= 4;
+  x /= 2;
+
+  printf("%d\n", x);
 
   return 0;
 }
