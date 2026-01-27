d966c88a608e55c344cc8a6e672001a6a455a570
myjourneyspeaks <lindelwekhumalo@outlook.com>
2026-01-27 14:04:10 +0200

    project five

--- projects/5. Weight Convertor/main.c
+++ projects/5. Weight Convertor/main.c
@@ -1,11 +1,18 @@
+/*
+ * Program: weightconvertor.c
+ * Purpse: demonstrating how to convert weight from kilograms to pounds and vise
+ * versa
+ * Author: Vusumuzi Lindelwe
+ * Date: 2026-01-27
+ */
+
 #include <stdio.h>
 
-int main() {
+int main(void) {
 
   // Weight convertor
   int choice = 0;
-  float kilogram = 0.0f;
-  float pound = 0.0f;
+  double kilogram = 0.0, pound = 0.0;
 
   printf("Weight Conversion Calculator\n");
   printf("1. Kilograms to Pounds\n");
@@ -16,13 +23,13 @@ int main() {
   if (choice == 1) {
     // Kilograms to Pounds
     printf("Enter the weight in Kilograms: ");
-    scanf("%f", &kilogram);
+    scanf("%lf", &kilogram);
     pound = kilogram * 2.20462;
     printf("%.2f kilograms is equal to %.2f pounds\n", kilogram, pound);
   } else if (choice == 2) {
     // Pounds to Kilograms
     printf("Enter the weight in Pounds: ");
-    scanf("%f", &pound);
+    scanf("%lf", &pound);
     kilogram = pound / 2.20462;
     printf("%.2f pounds is equal to %.2f kilogram\n", pound, kilogram);
   } else {
