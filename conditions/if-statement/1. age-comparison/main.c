f9d8a31a196ca4f439787c25678f5b26471ca86a
myjourneyspeaks <lindelwekhumalo@outlook.com>
2026-01-29 22:24:26 +0200

    if statement

--- conditions/if-statement/1. age-comparison/main.c
+++ conditions/if-statement/1. age-comparison/main.c
@@ -1,6 +1,7 @@
 /*
  * Program: agecom.c
- * Purpose: demonstrating how to use if statement
+ * Purpose: Reads a user's age and classifies it using if-else statements to
+ * demonstrate decision-making in C
  * Author: Vusumuzi Lindelwe
  * Date: 2026-01-29
  */
@@ -14,14 +15,14 @@ int main(void) {
   printf("How old are you? ");
   scanf("%d", &age);
 
-  if (age >= 65) {
-    printf("You are a senior\n");
-  } else if (age >= 18) {
-    printf("You are an adult\n");
-  } else if (age < 0) {
+  if (age < 0) {
     printf("You haven't been born yet\n");
   } else if (age == 0) {
     printf("You are a newborn\n");
+  } else if (age >= 65) {
+    printf("You are a senior\n");
+  } else if (age >= 18) {
+    printf("You are an adult\n");
   } else {
     printf("You are a child\n");
   }
